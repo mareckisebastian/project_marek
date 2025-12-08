5c04002bae9b14ffbbf4b7eb146783f6c8bf30bc
Sebastian Marecki <mareckisebastian.fullstack@gmail.com>
2025-12-08 10:19:53 +0100

    add image box component and enhance navigation menu styles

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 "use strict";
 
 const nav = document.querySelector("nav");
+// mobile menu toggle
+const menuIcon = document.querySelector(".material-symbols");
+const menuList = document.querySelector(".lista_menu");
 
 const handleScroll = () => {
     if (window.scrollY > 100) {
@@ -11,3 +14,33 @@ const handleScroll = () => {
 };
 
 window.addEventListener("scroll", handleScroll);
+
+//rozwijanie i zwijanie menu
+menuIcon.addEventListener("click", () => {
+    menuList.classList.toggle("open");
+    menuIcon.classList.toggle("active");
+    menuIcon.setAttribute(
+        "aria-expanded",
+        menuList.classList.contains("open")
+    );
+});
+
+// zamykanie menu po kliknięciu w link
+document.querySelectorAll(".lista_menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        menuList.classList.remove("open");
+        menuIcon.classList.toggle("active");
+    });
+});
+
+// reset menu when switching to desktop view
+window.addEventListener("resize", () => {
+    if (window.innerWidth >= 1024) {
+        menuList.classList.remove("open");
+        menuIcon.classList.remove("active");
+    }
+});
+
+
+
+
